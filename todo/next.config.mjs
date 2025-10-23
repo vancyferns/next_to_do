a1f21d772a50e4db4a646d00a0a00ca1522b7aa5
vancyferns <135420254+vancyferns@users.noreply.github.com>
2025-10-23 05:36:51 +0000

    updated next.config.js

--- todo/next.config.mjs
+++ todo/next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  
+  // 1. Enable static export
+  output: 'export',
+  
+  // 2. Set the basePath
+  // REPLACE '<your-repo-name>' WITH YOUR REPO NAME
+  basePath: process.env.NODE_ENV === 'production' ? '/next_to_do' : '',
+  
+  // 3. Disable image optimization
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
